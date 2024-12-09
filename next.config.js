a1b3936057e5ffd08d458f0d94336e1f53740505
bilalrasool123 <47560440+bilalrasool123@users.noreply.github.com>
2024-12-09 09:59:50 +0500

    next.config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   i18n: {
-    locales: ['en', 'de'], // List the supported languages
-    defaultLocale: 'de',    // Set the default language
+    locales: ['en', 'de'],       // List of supported languages
+    defaultLocale: 'de',         // Default language
+    localeDetection: false,      // Disable automatic language detection from URL or browser
   },
 };
